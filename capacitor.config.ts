7465146194299b0473c46abd2820885aa16bb31e
Ghs Julian <98700347+ghsjulian@users.noreply.github.com>
2026-06-12 07:57:45 +0000

    Addd Final Fixeses

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+   appId: "com.doctor.seba",
+  appName: "doctor-seba",
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
